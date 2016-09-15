64648b3dd2dda0ffdc1ea080052844f76166333d
zzd1989812 <zzdjavajob@163.com>
2016-09-16 02:16:01 +0800

    commit menus
    commit menus

--- cms/src/main/webapp/js/menu.js
+++ cms/src/main/webapp/js/menu.js
@@ -26,6 +26,46 @@ define(function () {
                 }
             ]
         },{
+            "text": "物流管理",
+            "nodes": [
+                {
+                    "text": "物流公司管理",
+                    "href": "#/company-list"
+                },
+                {
+                    "text": "线路管理",
+                    "href": "#/line-list"
+                },
+                {
+                    "text": "订单管理",
+                    "href": "#/order-list"
+                },
+                {
+                    "text": "配送管理",
+                    "href": "#/service-order-list"
+                }
+            ]
+        },{
+            "text": "门户管理",
+            "nodes": [
+                {
+                    "text": "资源管理",
+                    "href": "#/frontend-sys-res"
+                },
+                {
+                    "text": "流量管理",
+                    "href": "#/access-count-list"
+                },
+                {
+                    "text": "事件管理",
+                    "href": "#/events-list"
+                },
+                {
+                    "text": "日志管理",
+                    "href": "#/frontend-sys-log-list"
+                }
+            ]
+        },{
             "text": "日志管理",
             "nodes": [
                 {
